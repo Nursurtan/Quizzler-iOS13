155d5fff5b8bd024f9e31fd8c07a19cfb8c71b64
Nursurtan <arapovnur23@gmail.com>
2021-12-24 23:57:39 +0700

    Created a Question Struct

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,27 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by нурсултан арапов on 12/24/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question {
+    let text: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        text = q
+        answer = a
+        
+        
+        
+        
+    }
+    
+    
+    
+}
+
